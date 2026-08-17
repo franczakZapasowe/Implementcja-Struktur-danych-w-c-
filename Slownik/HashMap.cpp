0977849d42dd1745197bfc6bdd137e079720bb12
franczakZapasowe <russeldurantmvp@gmail.com>
2026-08-17 14:45:02 +0200

    Implementacja Kopiujacego operatora przypisania i destruktora

--- Slownik/HashMap.cpp
+++ Slownik/HashMap.cpp
@@ -48,25 +48,58 @@ HashMap::HashMap(const HashMap &h):m_rozmiar(h.m_rozmiar),m_liczbaElementow(h.m_
 
 HashMap & HashMap::operator=(const HashMap &h) {
     if (this!=&h) {
-        //sprzatanie
-        for (int i=0; i<m_rozmiar; i++) {
+        // najpierw usuwam wszystkie elementy aktualnej tablicy
+        for (int i =0; i<m_rozmiar; i++) {
             if (m_tablica[i]!=nullptr) {
-                Wezel*wskDoUsuniecia = m_tablica[i];
-                Wezel*wskNastepny = h.m_tablica[i];
-                while (wskDoUsuniecia->next!=nullptr) {
-                    delete wskDoUsuniecia;
-
+                Wezel *wskAktualnyDoUsun = m_tablica[i];
+                Wezel *wskNastepny = m_tablica[i]->next;
+                while (wskAktualnyDoUsun!=nullptr) {
+                    delete wskAktualnyDoUsun;
+                    wskAktualnyDoUsun = wskNastepny;
+                    wskNastepny = wskNastepny->next;
                 }
-
             }
         }
+        // teraz musimy przekopiowac wartosci
+        delete [] m_tablica;
+        m_tablica = nullptr;
+        m_liczbaElementow = h.m_liczbaElementow;
+        m_rozmiar = h.m_rozmiar;
+        m_tablica = new Wezel *[m_rozmiar];
+        for (int i=0; i<h.m_rozmiar; i++) {
+            if (h.m_tablica[i]!=nullptr) {
+                m_tablica[i] = new Wezel;
+                m_tablica[i]->klucz = h.m_tablica[i]->klucz;
+                m_tablica[i]->wartosc = h.m_tablica[i]->wartosc;
 
+                Wezel *aktualny = m_tablica[i];
+                Wezel *wskNastepny = m_tablica[i]->next;
+                while (wskNastepny!=nullptr) {
+                    aktualny->next = new Wezel;
+                    aktualny = aktualny->next;
+                    aktualny->klucz = wskNastepny->klucz;
+                    aktualny->wartosc = wskNastepny->wartosc;
+                    wskNastepny = wskNastepny->next;
+                }
+            }
+        }
     }
     return *this;
 }
 
 HashMap::~HashMap() {
-
+    for (int i =0; i<m_rozmiar; i++) {
+        if (m_tablica[i]!=nullptr) {
+            Wezel* wskAktuanyDoUsun = m_tablica[i];
+            Wezel* wskNastepny = m_tablica[i]->next;
+            while (wskAktuanyDoUsun!=nullptr) {
+                delete wskAktuanyDoUsun;
+                wskAktuanyDoUsun = wskNastepny;
+                wskNastepny = wskNastepny->next;
+            }
+        }
+    }
+    delete [] m_tablica;
 }
 
 
@@ -79,11 +112,8 @@ size_t HashMap::hash(std::string str) {
     return hash;
 }
 
-
-
-
 void HashMap::insert(std::string str,double wartosc) {
-
+    
 }
 
 void HashMap::serch() {
